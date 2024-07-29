86c9bd2418cde4cad9b2e506bc4e54f3bf8c7e94
Maria Dalva <dalva.maria.mos@gmail.com>
2024-07-29 14:09:26 -0300

    testes

--- tests/test_search.py
+++ tests/test_search.py
@@ -2,18 +2,12 @@ import unittest
 from src.search import search_book
 
 class TestSearchBook(unittest.TestCase):
-    def test_search_book_found(self):
-        isbn = "978-0-13-110362-7"  
-        result = search_book(isbn)
-        self.assertIsNotNone(result['gerente'])
-        self.assertEqual(result['gerente']['title'], "Livro D")
-        
-    def test_search_book_not_found(self):
-        isbn = "000-0-00-000000-0"
-        result = search_book(isbn)
-        self.assertIsNone(result['bibliotecária'])
-        self.assertIsNone(result['gerente'])
+    def test_search_book(self):
+        isbn_to_search = "978-0-13-110362-7"
+        result = search_book(isbn_to_search)
+        self.assertIsNotNone(result["bibliotecária"])
+        self.assertIsNotNone(result["gerente"])
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
 
